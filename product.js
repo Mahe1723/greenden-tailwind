7dc65fe69a098e6df4eaf9f2ad1daad4a086e23b
Mahe1723 <mahe17rjm@gmail.com>
2023-12-05 11:42:37 +0530

    Added js

---
+++ product.js
@@ -0,0 +1,39 @@
+//selecting side navbar,menu icon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click",function()
+{
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+
+//product search functionality
+
+
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("Search")
+var productlist = container.querySelectorAll("div")
+
+    search.addEventListener("keyup",function(){
+        var enteredvalue = event.target.value.toUpperCase()
+
+        for(count = 0; ount<productlist.length; count=count+1)
+        {
+            var productname = productlist[count].querySelector("h1").textContent
+
+            if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+        }
+    })
